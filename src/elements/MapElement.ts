ce70b96406862db80f24c21a2e27810c51afcbef
Ricardo Gallardo <rgallardouk@gmail.com>
2021-02-04 17:18:07 +0000

    feat: Add map marker for map element

--- src/elements/MapElement.ts
+++ src/elements/MapElement.ts
@@ -9,6 +9,8 @@ export default class ObserverMapElement implements Observer {
 
   private map?: google.maps.Map
 
+  private marker?: google.maps.Marker
+
   constructor(private mapElement: HTMLElement) {
     this.apiKey = process.env.API_KEY as string
     if (this.apiKey) {
@@ -41,14 +43,41 @@ export default class ObserverMapElement implements Observer {
     return value
   }
 
-  onNotify(s: Subject): void {
+  markerLoad = async (
+    markerOptions: google.maps.MarkerOptions,
+  ): Promise<void | google.maps.Map<Element> | google.maps.StreetViewPanorama | null | undefined> => {
+    const value = this.loader
+      .load()
+      .then(() => {
+        this.marker = new google.maps.Marker(markerOptions)
+        return this.marker.getMap()
+      })
+      .catch((err: unknown) => {
+        if (err instanceof Error) {
+          // eslint-disable-next-line no-console
+          console.error(err.message)
+        } else {
+          // eslint-disable-next-line no-console
+          console.error(String(err))
+        }
+      })
+    return value
+  }
+
+  async onNotify(
+    s: Subject,
+  ): Promise<void | google.maps.Map<Element> | google.maps.StreetViewPanorama | null | undefined> {
     if (s instanceof MapQuery && s.addresses.length > 0) {
       const {
-        geometry: {
-          location: { lat, lng },
-        },
+        geometry: { location },
+        formatted_address: formatedAdress,
       } = s.addresses[0]
-      this.mapLoad({ center: { lat, lng }, zoom: 16 })
+      const element = await this.mapLoad({ center: location, zoom: 16 })
+      if (element instanceof google.maps.Map || element instanceof google.maps.StreetViewPanorama) {
+        this.markerLoad({ label: formatedAdress, position: location, map: element })
+      }
+      return element
     }
+    return undefined
   }
 }
